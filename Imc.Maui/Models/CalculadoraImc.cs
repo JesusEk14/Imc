35d811cc07a95117630a022b99c566f2b7b9a806
Jesus Alejandro Ek Koyoc <24470308@campeche.tecnm.mx>
2026-09-22 12:23:56 -0600

    Versión funcional completa inicial

---
+++ Imc.Maui/Models/CalculadoraImc.cs
@@ -0,0 +1,33 @@
+﻿namespace Imc.Maui.Models
+{
+    public static class CalculadoraImc
+    {
+        public static decimal IndiceDeMasaCorpotal(
+            decimal peso, decimal estatura)
+        {
+            return peso / (estatura * estatura);
+        }
+
+        public static string SituacionNutricional(
+            decimal imc)
+        {
+            if (imc < 18.5m)
+            {
+                return "Peso Bajo";
+            }
+            if (imc < 25.0m)
+            {
+                return "Peso Normal";
+            }
+            if (imc < 30.0m)
+            {
+                return "Sobrepeso";
+            }
+            if (imc < 40.0m)
+            {
+                return "Obesidad";
+            }
+            return "Obesidad Extrema";
+        }
+    }
+}
